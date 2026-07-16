2b879fae8a0656f58f9caa4f4bbaeb3e0449a87c
ayomikun286 <edegbaiayomikun@gmail.com>
2026-07-16 22:41:55 +0100

    fix

--- script/config.js
+++ script/config.js
@@ -1,2 +1,2 @@
-export const API = "http://localhost:5000/";
-// export const API = "https://paveway-server.onrender.com/"
+// export const API = "http://localhost:5000/";
+export const API = "https://paveway-server.onrender.com/"
